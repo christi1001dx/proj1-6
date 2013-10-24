9b8b354f69a76135e8c1a764761226c2cec8ad7f
arfan308 <areha308@gmail.com>
2013-10-24 12:24:07 -0400

    Outline app

--- ARSS/app.py
+++ ARSS/app.py
@@ -6,9 +6,38 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	pass
+    if (username = session["username"]):      
+        return render_template("index.html")
+    else:
+        return redirect('/login')
 
+@app.route('/story')
+def story():
+    
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if (request.method == "GET"):
+        return render_template("login.html")
+    else:
+        username = request.form["username"].encode("ascii", "ignore")
+        password = request.form["password"].encode("ascii", "ignore")
+        
+@app.route('/register', methods = ['GET', 'POST'])
+def register('/register'):
+    if (request.method == "GET"):
+        return render_template("register.html")
+    else:
+        username = request.form["username"].encode("ascii", "ignore")
+        password = request.form["password"].encode("ascii", "ignore")
+
+@app.route("/logout")
+def logout():
+    if "username" in sesson:
+        session.pop("username")
+        return render_template("logout.html")
+    else:
+        return redirect("/")
+        
 if __name__ == '__main__':
-	app.jinja_env.line_statement_prefix = '='
 	app.debug = True
 	app.run(host='0.0.0.0')
