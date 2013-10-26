daeb8b91b4d93a846061b187033af5400e75dfa2
Jasper Lu <repsajrules@gmail.com>
2013-10-26 08:53:51 -0700

    finisehd the showing blog posts thing

--- CCLL/SubmitBlogPage/utils.py
+++ CCLL/SubmitBlogPage/utils.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from datetime import date
 
 def auth (user, passwd, coll):
     return [x for x in coll.find({'username': user, 'password': passwd})] != []
@@ -6,8 +7,9 @@ def auth (user, passwd, coll):
 def addUser (user, passwd, coll):
     coll.insert({'username': user, 'password': passwd})
 
-def addPost (user, date, text, coll):
-    coll.insert({'username': user, 'date': date, 'text': text})
+def addPost (user, title, text, coll):
+    coll.insert({'username': user, 'date': str(date.today()), 'title': title, 'text': text})
 
 def getPosts (user, coll):
-    return [x for x in coll.find({'username':user}).sort({'date':-1})]
+    return [x for x in coll.find({'username':'jasper'}).sort([('date',-1)])]
+
