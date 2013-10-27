66c4bd70e684a387e09bbbf46a98b97a801d2056
Jasper Lu <repsajrules@gmail.com>
2013-10-27 16:35:40 -0700

    new to oldest

--- CCLL/utils.py
+++ CCLL/utils.py
@@ -8,8 +8,8 @@ def addUser (user, passwd, coll):
     coll.insert({'username': user, 'password': passwd})
 
 def addPost (user, title, genre, text, coll):
-    coll.insert({'username': user, 'date': strftime("%Y-%M-%d %H:%M"), 'title': title, 'genre': genre, 'text': text})
+    coll.insert({'username': user, 'date': strftime("%Y-%M-%d %H:%M:%S"), 'title': title, 'genre': genre, 'text': text})
 
 def getPosts (user, coll):
-    return [x for x in coll.find({'username':user}).sort([('date',1)])]
+    return [x for x in coll.find({'username':user}).sort([('date',-1)])]
 
