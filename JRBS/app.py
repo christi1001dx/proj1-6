dd2925938fb0fb0a5ddceaa80151d734b133f18b
rlamchops <lamraymond1996@gmail.com>
2013-11-01 20:48:55 -0400

    small updates, haven't tested yet though

--- JRBS/app.py
+++ JRBS/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, session, url_for
+import sqlite3
 
 app = Flask(__name__)
 app.secret_key="JRBS"
@@ -19,35 +20,56 @@ def signup():
         box = request.form["acceptTerms"]
         if button == "Login":
             if not box:                
-                return render_template("register.html", "you need to check the box first")
-            elif auth.auth(username, password) :
+                return render_template("register.html", message = "you need to check the box first")
+            elif stuff.check(username) :
                 session["name"] = username
                 return redirect(url_for('posts'))
             else:
-                return render_template("register.html", "wrong username/pw combo")   
+                return render_template("register.html", message = "wrong username/pw combo")   
         
 @app.route("/new")
 def new():
-    return "<h1>New posts made here</h1>"
+    button = request.form["cancel"]
+    if request.method=="GET":
+        return render_template("new.html")
+    elif button:
+        return redirect(url_for('posts'))
+    else:
+        title = request.form["title"]
+        post = request.form["post"]
+        stuff.add(title, post)
+        return redirect(url_for('posts'))
 
-@app.route("/archive")
-def archive():
-    if request.method=="GET":        
-        return "<h1>List of all posts here</h1>"
+#@app.route("/archive")
+#def archive():                     ****don't really see the difference b/w this and /posts, so will
+#    if request.method=="GET":        comment this out for now******
+#        return render_template
 
-@app.route("/admin")
-def admin():
+#@app.route("/admin") ******leaving this blank for now. saving for later.*****
+#def admin():
 
 @app.route("/posts")
 def posts():
+    button = request.form["users"]
+    page = request.form["pagenumber"]
+    if request.method=="GET":
+        return render_template("posts.html")
+    elif button:
+        if 'username' in session:
+            user_id = stuff.getUser()
+            return redirect(url_for('posts/user/{user_id}'))
+        else
+            page_number = page
+            return redirect(url_for('posts/{page_number}')
 
 @app.route("/posts/{page_number}")
 def posts():
+    button = request.form["home"]
     if request.method=="GET":
-        return render_template("")
+        return render_template("users")
     else 
-    return "<h1> should be able to navigate through posts, comments, etc. </h1>"
-
+        return "<h1> should be able to navigate through posts, comments, etc. </h1>"
+    
 @app.route("/posts/user/{user_id}")
 
 @app.route("/posts/user/{user_id}/{page_number}")
