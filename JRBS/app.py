8c3d317398b3e1eb6b64e7fe5d97f36ab5596b1f
rlamchops <lamraymond1996@gmail.com>
2013-11-02 17:40:56 -0400

    added login, logout, changed some stuff. did not test yet though

--- JRBS/app.py
+++ JRBS/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template, redirect, session, url_for
+from database import User, Post, Comment, Database
 import sqlite3
 
 app = Flask(__name__)
@@ -8,6 +9,20 @@ app.secret_key="JRBS"
 def home():
     return redirect(url_for('posts'))
 
+@app.route("/login")
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        username = request.form["name"].encode("ascii", "ignore")
+        password = request.form["password"].encode("ascii", "ignore")
+        verify = Database(name, password)
+        answer = verify.login()
+        if not answer == "ok":
+            return render_template("login.html", message = "Username does not exist or incorrect user/pw combination")
+        else:
+            redirect(url_for('posts'))
+
 @app.route("/signup")
 def signup():
     if request.method=="GET":
@@ -16,28 +31,30 @@ def signup():
         username = request.form["name"].encode("ascii", "ignore")
         password1 = request.form["password1"].encode("ascii", "ignore")
         password2 = request.form["password2"].encode("ascii", "ignore")
+        displayname = request.form["displayname"].encode("ascii","ignore")
         button = request.form["button"]
         box = request.form["acceptTerms"]
         if button == "Register":
             if not box:                
                 return render_template("register.html", message = "you need to check the box first")
-            elif stuff.check(username) :
+            elif stuff.check(username) : #check will return true if  username is not found, false otherwise
                 session["name"] = username
+                add = Database(username, password)
+                add.register()
                 return redirect(url_for('posts'))
-            else:
-                return render_template("register.html", message = "wrong username/pw combo")   
         
 @app.route("/new")
 def new():
-    button = request.form["cancel"]
+    button1 = request.form["cancel"]
+    button2 = request.form["submit"]
+    title = request.form["title"]
+    post = request.form["post"]
     if request.method=="GET":
         return render_template("new.html")
-    elif button:
+    elif button1:
         return redirect(url_for('posts'))
-    else:
-        title = request.form["title"]
-        post = request.form["post"]
-        stuff.add(title, post)
+    elif button2:
+        #add = Post() *****what to add in the parenthese...hmmmm....
         return redirect(url_for('posts'))
 
 #@app.route("/archive")
@@ -57,10 +74,10 @@ def posts():
     elif button:
         if 'username' in session:
             user_id = stuff.getUser()
-            return redirect(url_for('posts/user/{user_id}'))
+            return redirect(url_for('posts/user/{user_id}' % user_id))
         else
             page_number = page #don't think this works, but putting it here anyway
-            return redirect(url_for('posts/{page_number}')
+            return redirect(url_for('posts/{page_number}' % page)
 
 @app.route("/posts/{page_number}")
 def posts():
@@ -72,7 +89,7 @@ def posts():
         return redirect(url_for('posts')
     else
         page_number = page
-        return redirect(url_for('posts/{page_number}')
+        return redirect(url_for('posts/{page_number}' % page)
     
 @app.route("/posts/user/{user_id}")
 def posts():
@@ -84,7 +101,7 @@ def posts():
         return redirect(url_for('posts')
     else 
         page_number = page
-        return redirect(url_for('posts/user/{user_id}/{page_number}'))
+        return redirect(url_for('posts/user/{user_id}/{page_number}' % page))
 
 @app.route("/posts/user/{user_id}/{page_number}")
 def posts():
@@ -97,3 +114,9 @@ def posts():
     else
         page_number = page
         return redirect(url_for('posts/user/{user_id}/{page_number}'))
+
+@app.route("/logout")
+def logout():
+    if 'username' in session:
+        session.pop('username', None)
+    return redirect('posts')
