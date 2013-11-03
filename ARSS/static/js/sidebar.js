83d7782fc38f58ae84705a48050da7881393a9c9
Steve Zhu <st.zhu1@gmail.com>
2013-11-03 15:21:26 -0500

    Sidebar widths as variables

--- ARSS/static/js/sidebar.js
+++ ARSS/static/js/sidebar.js
@@ -1,10 +1,17 @@
 (function($) {
+	function add(a, b) {
+		return parseInt(a, 10) + parseInt(b, 10) + "px";
+	}
 	$(document).ready(function() {
 		var $body = $("body");
-		var sidebarWidth = "250px";
+		var sidebarLabelWidth = $(".sidebar-label").css("width");
+		var sidebarBodyWidth = "250px";
+		// total width
+		var sidebarWidth = add(sidebarLabelWidth, sidebarBodyWidth);
+
 		$(".sidebar").hover(function() {
-			$(".sidebar-body-wrapper").css("width", sidebarWidth)
-			$body.css("padding-left", "285px");
+			$(".sidebar-body-wrapper").css("width", sidebarBodyWidth)
+			$body.css("padding-left", sidebarWidth);
 		}, function() {
 			$(".sidebar-body-wrapper").css("width", "")
 			$body.css("padding-left", "");
