9f2b931c9539c85488ce667380fdcee04f23ebda
Roger Li <rog3rli@gmail.com>
2013-11-03 17:17:20 -0500

    login+register functions for steefu

--- ARSS/static/js/login.js
+++ ARSS/static/js/login.js
@@ -1,10 +1,28 @@
 
-$(function() {
-	
-	
-	
-	
-	
-});
+// callback(success)
+//   success = T/F
+function login(user, pass, callback) {
+	$.post("login", {
+		username : user,
+		password : pass
+	}, function(d) {
+		if (d.indexOf("success") > -1) callback(true);
+		else callback(false);
+	});
+}
+
+// callback(success, error)
+//    success = T/F
+//    error = whatever
+function register(user, pass, pass2, callback) {
+	$.post("register", {
+		username : user,
+		password : pass,
+		password2 : pass2
+	},function(d) {
+		if (d.indexOf("success") > -1) callback(true);
+		else callback(false, d);
+	});
+}
 
 
