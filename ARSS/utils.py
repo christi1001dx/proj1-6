fc4b00e1b49e2a15c4721de12943013abd0b3b04
arfan308 <areha308@gmail.com>
2013-11-02 22:25:34 -0400

    Add register.html

--- ARSS/utils.py
+++ ARSS/utils.py
@@ -81,11 +81,11 @@ def get_collection():
 # user must type password 2 times to make account
 def add_user(username, password, password2):
     if (get_collection().find_one({'username': username}, fields = {'_id': False})):
-        return "copy"
+        return "already exists"
     elif (password.__len__() < 4):
         return "short password"
     elif (password != password2):
-        return "retype: passwords mismatch"
+        return "retype: password don't match"
     else:
         get_collection().insert({'username': username, 'password': password})
         return "good job"
@@ -100,9 +100,9 @@ def account_exists(username, password):
 # used to change password to password2
 def change_password(username, password, password2):
     if (password.__len__() < 4):
-        return "too short"
+        return False
     elif (password != password2):
-        return "retype: password mismatch"
+        return False
     else:
         get_collection().update({'username': username}, {'$set':{'password': password}})
-        return "successfully changed password" 
+        return True
