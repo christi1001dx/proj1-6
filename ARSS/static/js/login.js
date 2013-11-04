87f76bbb8f84f51a374312b2a6eb1ac6a36ac7b5
Sweyn Venderbush <sweyn3@yahoo.com>
2013-11-03 23:16:47 -0500

    function edits

--- ARSS/static/js/login.js
+++ ARSS/static/js/login.js
@@ -60,10 +60,11 @@ function add_line(title, line, callback) {
 	});
 }
 
+
 function get_all_stories() {
-	$.get("allstories", function(d) {
-    var allstories = JSON.parse(d);
-	});
+        $.get("allstories", function(d) {
+            window.allstories = JSON.parse(d);
+        });
 }
 
 
