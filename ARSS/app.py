1181be4a97011257f3869f96108165e21e9b29a5
Steve Zhu <st.zhu1@gmail.com>
2013-11-03 22:19:01 -0500

    Serializing unique id fix

--- ARSS/app.py
+++ ARSS/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask import request, render_template, redirect, session, url_for
+from bson import json_util
 import utils
 import helpers
 import json
@@ -15,79 +16,81 @@ env.line_statement_prefix = 'yolo'
 env.globals.update(helpers=helpers)
 
 def get_form_value(key):
-    return request.form[key].encode("ascii", "ignore")
+	return request.form[key].encode("ascii", "ignore")
 
 @app.route('/test')
 def test():
-    return render_template('test.html')
+	return render_template('test.html')
 
 @app.route('/story')
 def story_test():
-    return render_template('story_test.html')
+	return render_template('story_test.html')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
 
 @app.route('/story/<name>', methods = ['GET'])
 def get_story(name):
-    print json.dumps(list((utils.return_all_lines(name))))
-    return 'hi'
+	print json.dumps((utils.return_all_lines(name)), default=json_util.default)
+	return 'hi'
 
 @app.route('/addline', methods=['POST'])
 def add_line():
-    if not "username" in session:
-	    return "login"
-    author = session["username"]
-    title = get_form_value('title')
-    line = get_form_value('line')
-    if utils.add_line(line, title, author):
-	    return "success"
-    else:
-        return "error"
+	if not "username" in session:
+		return "login"
+	author = session["username"]
+	title = get_form_value('title')
+	line = get_form_value('line')
+	if utils.add_line(line, title, author):
+		return "success"
+	else:
+		return "error"
 
 @app.route('/makestory', methods=['POST'])
 def make_story():
-    if not "username" in session:
-	    return "login"
-    author = session["username"]
-    title = get_form_value('title')
-    return str(utils.make_story(title, author, False))
+	if not "username" in session:
+		return "login"
+	author = session["username"]
+	title = get_form_value('title')
+	return str(utils.make_story(title, author, False))
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    if (request.method == "GET"):
-        return render_template("login.html")
-    else:
-        username = get_form_value('username')
-        password = get_form_value('password')
-        if (utils.account_exists(username, password)):
-            session["username"] = username
-            return "success,"+username.decode("utf-8")
-        else:
-            return "fail,"
+	if (request.method == "GET"):
+		return render_template("login.html")
+	else:
+		username = get_form_value('username')
+		password = get_form_value('password')
+		if (utils.account_exists(username, password)):
+			session["username"] = username
+			return "success,"+username.decode("utf-8")
+		else:
+			return "fail,"
 
 @app.route('/register', methods = ['GET', 'POST'])
 def register():
-    if (request.method == "GET"):
-        return render_template("register.html")
-    else:
-        username = get_form_value('username')
-        password = get_form_value('password')
-        password2 = get_form_value('password2')
-        registertry = utils.add_user(username, password, password2)
-        if (registertry == "good job"):
-            return "success,"+username.decode("utf-8")
-        else:
-            return registertry
+	if (request.method == "GET"):
+		return render_template("register.html")
+	else:
+		username = get_form_value('username')
+		password = get_form_value('password')
+		password2 = get_form_value('password2')
+		registertry = utils.add_user(username, password, password2)
+		if (registertry == "good job"):
+			return "success,"+username.decode("utf-8")
+		else:
+			return registertry
 
 @app.route("/logout")
 def logout():
-    if "username" in session:
-        session.pop("username")
-        return "success"
-    else:
-        return "ok"
+	if "username" in session:
+		session.pop("username")
+		return "success"
+	else:
+		return "ok"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+	#utils.make_story('test', 'me', False)
+	#utils.add_line("test line", 'test', 'me')
+	app.run(host='0.0.0.0')
