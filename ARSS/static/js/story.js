95fc9b33a9fd3cd9fc76e320b03bcb0df52e9261
Steve Zhu <st.zhu1@gmail.com>
2013-11-04 01:41:27 -0500

    Implemented add lines

--- ARSS/static/js/story.js
+++ ARSS/static/js/story.js
@@ -10,44 +10,60 @@
 		}
 		return t.toString();
 	};
+	$.fn.toggleEmpty = function(empty) {
+		var placeholder = this.data("placeholder");
+		if (empty) {
+			this.data("empty", true);
+			this.text(placeholder);
+			this.css("color", "gray");
+		} else {
+			this.data("empty", false);
+			this.text("");
+			this.css("color", "");
+		}
+		return empty;
+	};
+	$.fn.cleanAttrs = function() {
+		this.removeAttr("id");
+		this.removeAttr("contenteditable");
+		this.removeAttr("style");
+		this.removeAttr("data-placeholder");
+	};
 	/*** NEW LINE CODE ***/
 	$(document).ready(function() {
-		var $newLine = $("#story-new-line");
-		var placeholder = $newLine.data("placeholder");
-		$newLine.toggleEmpty = function(empty) {
-			if (empty) {
-				$newLine.data("empty", true);
-				$newLine.text(placeholder);
-				$newLine.css("color", "gray");
-			} else {
-				$newLine.data("empty", false);
-				$newLine.text("");
-				$newLine.css("color", "");
-			}
-			return empty;
-		};
-		$newLine.toggleEmpty(true);
-		$newLine.on("keydown paste cut delete", function(e) {
-			var text = $newLine.text();
+		var $text = $(".story-text");
+		$text.on("keydown paste cut delete", "#story-new-line", function(e) {
+			var text = $(this).text();
 			var selection = $.getSelection();
 			if (e.keyCode == 8 && (text.length <= 1 || selection.length == text.length)) {
-				$newLine.toggleEmpty(true);
+				$(this).toggleEmpty(true);
 			}
-		}).on("keypress", function(e) {
+		}).on("keypress", "#story-new-line", function(e) {
+			$this = $(this);
 			// if is letter or punctuation
-			if (e.keyCode >= 32 && $newLine.data("empty")) {
-				$newLine.toggleEmpty(false);
+			if (e.keyCode >= 32 && $this.data("empty")) {
+				$this.toggleEmpty(false);
 			}
 			// if enter key
 			else if (e.keyCode == 13) {
-				//e.preventDefault();
-				add_line($("#story-new-line"),$(".story-container h1").first().text());
+				e.preventDefault();
+				var title = $(".story-container h1").first().text();
+				var line = $this.text();
+				add_line(title, line, function(success) {
+					if (success) {
+						$(".story-text .text").append(line + " ");
+						$this.after('<span id="story-new-line" contenteditable=true data-placeholder="Continue the story!"></span>');
+						$("#story-new-line").toggleEmpty(true);
+					}
+				});
 			}
-		}).on("paste", function(e) {
+		}).on("paste", "#story-new-line", function(e) {
+			var $this = $(this);
 			var clipboard = e.originalEvent.clipboardData.getData("Text");
-			if (clipboard.length > 0 && $newLine.data("empty")) {
-				$newLine.toggleEmpty(false);
+			if (clipboard.length > 0 && $this.data("empty")) {
+				$this.toggleEmpty(false);
 			}
 		});
+		$("#story-new-line").toggleEmpty(true);
 	});
 })(jQuery);
