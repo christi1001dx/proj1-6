29ecb1af25e1de6b5aae0b254e8eb1b83b3af206
thull <thull@trollz.(none)>
2013-11-03 21:00:39 -0500

    changed stuff in mangodb.py, should work

--- EHTT/erutil.py
+++ EHTT/erutil.py
@@ -1,11 +1,16 @@
 
-errorbox = {'err0':"Field missing",
-            'err1':"Error101: User not found",
-            'err2':"Username or password missing"
-            'err3':"Username exists"
-            'err4':"Error404: Post not found"
-            'err5':"Post name already exists"
+error = -1
+errorbox = {-1:"no error",
+             0:"Field missing",
+             1:"Error101: User not found",
+             2:"Username or password incorrect",
+             3:"Username exists",
+             4:"Error404: Post not found",
+             5:"Post name already exists",
 }
 
 def errorlook():
     return errorbox[error]
+def errorch(num):
+    error = num
+    print(error)
