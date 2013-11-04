7ff51dc4f0d885197069e7012349bf6a13861a4f
Ben Kurtovic <ben.kurtovic@gmail.com>
2013-11-04 01:20:01 -0500

    Finish other routes, I think.

--- JRBS/app.py
+++ JRBS/app.py
@@ -43,78 +43,41 @@ def signup():
         return redirect("/posts")
     return render_template("register.html", error=answer)
 
-@app.route("/new")
-def new():
-    button1 = request.form["cancel"]
-    button2 = request.form["submit"]
-    title = request.form["title"]
-    post = request.form["post"]
-    if request.method=="GET":
-        return render_template("new.html")
-    elif button1:
-        return redirect(url_for('posts'))
-    elif button2:
-        stuff.add(title, post)
-        #possibly create a Post and add it to db? coming back to this later.
-        return redirect(url_for('posts'))
+# @app.route("/new")
+# def new():
+#     button1 = request.form["cancel"]
+#     button2 = request.form["submit"]
+#     title = request.form["title"]
+#     post = request.form["post"]
+#     if request.method=="GET":
+#         return render_template("new.html")
+#     elif button1:
+#         return redirect(url_for('posts'))
+#     elif button2:
+#         stuff.add(title, post)
+#         return redirect(url_for('posts'))
 
 @app.route("/posts")
 def posts():
-    button = request.form["users"]
-    page = request.form["pagenumber"]
-    if request.method=="GET":
-        return render_template("posts.html")
-    elif button:
-        if 'username' in session:
-            user_id = stuff.getUser()
-            return redirect(url_for('posts/user/%s' % user_id))
-        else:
-            page_number = page
-            return redirect(url_for('posts/%s' % page)) #don't think this works, but putting it here anyway
+    return render_template("posts.html", posts=database.get_posts())
 
-@app.route("/posts/<page_number>")
-def posts_page(page_number=1):
-    button = request.form["home"]
-    page = request.form["pagenumber"]
-    if request.method=="GET":           #don't know how to make the page based on pg # here....
-        return render_template("posts") #so I'll just leave what I was trying to do below.
-        #pg = Database(None, page)
-        #return render_template("posts", posts = pg.get_posts()) ****get_posts() is from database.py******
-    elif button == "home":
-        return redirect(url_for('posts'))
-    else:
-        page_number = page
-        return redirect(url_for('posts/%s' % page))
+@app.route("/posts/<page>")
+def posts_page(page=1):
+    return render_template("posts.html", posts=database.get_posts(page=page))
 
-@app.route("/posts/user/<user_id>")
-def posts_user(user_id):
-    button = request.form["home"]
-    page = request.form["pagenumber"]
-    if request.method=="GET":
-        return render_template("posts") #same as above
-    elif button == "home":
-        return redirect(url_for('posts'))
-    else:
-        page_number = page
-        return redirect(url_for('posts/user/%s/%s' % (user, page)))
+@app.route("/posts/user/<user>")
+def posts_user(user):
+    return render_template("posts.html", posts=database.get_posts(user=user))
 
-@app.route("/posts/user/<user_id>/<page_number>")
-def posts_user_page(user_id, page_number=1):
-    button = request.form["home"]
-    page = request.form["pagenumber"]
-    if request.method=="GET":
-        return render_template("posts") # same as above
-    elif button == "home":
-        return redirect(url_for('posts'))
-    else:
-        page_number = page
-        return redirect(url_for('posts/user/%s/%s' % (user, page)))
+@app.route("/posts/user/<user>/<page>")
+def posts_user_page(user, page=1):
+    return render_template("posts.html",
+                           posts=database.get_posts(user=user, page=page))
 
 @app.route("/logout")
 def logout():
-    if 'username' in session:
-        session.pop('username', None)
-    return redirect('posts')
+    session.pop('username', None)
+    return redirect("/posts")
 
 if __name__=="__main__":
     app.debug=True
