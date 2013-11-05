082a8c18f2c92e50a4f3d9d7b12836a6e90b1499
Julie <zhu.julie2@gmail.com>
2013-11-05 01:14:35 -0500

    changed one name

--- JJJV/app.py
+++ JJJV/app.py
@@ -14,23 +14,22 @@ def get_form_value(key):
     return request.form[key].encode('ascii','ignore')
 
 def logged_in():
-	if 'username' in session and not username_exists(session['username']):
-		session.pop('username', None)
-	return 'username' in session and session['username'] != None
+    if 'username' in session and not utils.checkUser(session['username']):
+        session.pop('username', None)
+    return 'username' in session and session['username'] != None
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-        error = None
-        if request.method == 'POST':
-                username = get_form_value('username')
-                password = get_form_value('password')
-                if validate_user(username, password):
-                    session['username'] = username
+    error = None
+    if request.method == 'POST':
+        username = get_form_value('username')
+        password = get_form_value('password')
+        if utils.authenticate(username, password):
+            session['username'] = username
+            return redirect(url_for('blog'))
         else:
             error = 'Incorrect username or password.'
-            if logged_in():
-                return redirect(url_for('blog'))
-        return render_template('login.html', title='Login', error=error)
+    return render_template('login.html', title='Login', error=error)
                 
 @app.route('/logout')
 def logout():
@@ -39,36 +38,35 @@ def logout():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-        error = None
-        if request.method == 'POST':
-                username = get_form_value('username')
-                password = get_form_value('password')
-                password_confirm = get_form_value('password-confirm')
-                if checkUser(username):
-                        error = 'An account already exists with that username.'
-                elif password != password_confirm:
-                        error = 'The two passwords are not equal.'
-                else:
-                        register(username, password)
-                        session['username'] = username
-        if logged_in():
-                return redirect(url_for('blog'))
-        return render_template('register.html', title='Register', error=error)
+    error = None
+    if request.method == 'POST':
+        username = get_form_value('username')
+        password = get_form_value('password')
+        password_confirm = get_form_value('password-confirm')
+        if checkUser(username):
+            error = 'An account already exists with that username.'
+        elif password != password_confirm:
+            error = 'The two passwords are not equal.'
+        else:            
+            register(username, password)
+            session['username'] = username
+            return redirect(url_for('home'))
+    if logged_in():
+        return redirect(url_for('blog'))
+    return render_template('register.html', title='Register', error=error)
 
 @app.route('/blog')
 def blog():
-        return render_template('index.html', title='Blog',error=error)
+        return render_template('index.html')
 
 @app.route('/accounts')
 def accounts():
-        users = get_users_as_list()
-        a = ""
-        for user in users:
-                a += user + "<br>"
-        return a
-
+    users = get_users_as_list()
+    a = ""
+    for user in users:
+        a += user + "<br>"
+    return a
+        
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0',port=5000)
-    
-
